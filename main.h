450077b3748a04f085dd5b035adc7cc3af2cacf5
alexander katrompas <alexander.katrompas@austincc.edu>
2025-09-24 09:16:02 +0300

    added all include we know we need added all defines we will use for testing

--- main.h
+++ main.h
@@ -8,8 +8,17 @@
 #ifndef STACK_MAIN_H
 #define STACK_MAIN_H
 
+#define MULTIPLIER 1.5 // used to test overflow and underflow
+#define NARROW .30 // percent of the stack for a narrow band
+#define RANDOM_MULTIPLIER 10
+#define CHOICES 6 // used to pick random operations
+#define MAX_INT 100000 // random ints to push, 1 - 100000
 
 #include <iostream> // cout, endl
+#include <stdio.h>  // rand
+#include <time.h>   // time
+#include <cmath>    // round
+#include "stack.h"  // stack class
 #include "stack.h" // Stack
 
 using std::cout;
