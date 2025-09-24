f9ea66031b3cd752bbfcdfc212ca9e392f93f8a9
alexander katrompas <alexander.katrompas@austincc.edu>
2025-09-24 09:20:28 +0300

    completed overflow tests tests count and report logical occurrence of each operation

--- main.cpp
+++ main.cpp
@@ -105,7 +105,81 @@ int main() {
            cout << "failed" << endl;
        }
 
-    
-   return 0;
+    /************************************************
+    ******* OVERFLOW TESTS **************************
+    ************************************************/
+
+    cout << "  overflow tests ==> ";
+
+    // reset counters for tests
+    is_empty = 0;
+    peeked = 0;
+    popped = 0;
+    pushed = 0;
+    // fill stack
+    for (int i = 0; i <= STACKSIZE; i++) { // go one too far
+        value = rand()%2 ? -(i + 1) : i+1; // 50/50 negative or positive number
+        stack.push(value);
+    }
+
+    for (int i = 0; i < STACKSIZE*MULTIPLIER; i++) {
+
+        if (stack.isEmpty()) {
+            is_empty++;
+        } else {
+            // should always decrement
+            is_empty--;
+        }
+
+        if(stack.peek(&value)){
+            // should always increment
+            peeked++;
+        } else {
+            peeked--;
+        }
+
+        try{
+            // should always increment
+            // stack is now not full
+            value = stack.pop();
+            popped++;
+        } catch(...){
+            popped--;
+        }
+
+        if (stack.push(i)){
+            // should always increment
+            // stack is now full again
+            pushed++;
+        } else{
+            pushed--;
+        }
+
+        if (stack.push(i)){
+            pushed++;
+        } else{
+            // should always decrement
+            pushed--;
+        }
+    }
+
+    // INSTRUCTIONAL: notice these numbers are logical
+    // in overflow, we should expect these numbers
+    if(popped == round(STACKSIZE*MULTIPLIER) &&
+       peeked == round(STACKSIZE*MULTIPLIER) &&
+       pushed == 0 &&
+       -is_empty == round(STACKSIZE*MULTIPLIER)) {
+        cout << "pass" << endl;
+       } else {
+           cout << "failed" << endl;
+       }
+
+
+
+
+
+
+
+    return 0;
 }
 
