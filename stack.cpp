070c2aa8b5c2ab4c59c522649b254bb88c166012
alexander katrompas <alexander.katrompas@austincc.edu>
2025-09-24 09:05:41 +0300

    fixed indenting in stack.cpp

--- stack.cpp
+++ stack.cpp
@@ -35,70 +35,70 @@ Stack::~Stack() {
     *********************************************/
     }
 
-    bool Stack::isEmpty() {
-        /* ****************************************
-        isEmpty - is the stack empty?
+bool Stack::isEmpty() {
+    /* ****************************************
+    isEmpty - is the stack empty?
 
-        @param na : na
-        @return (bool) : true if empty, false if not
-        @exception na : na
-        @note the 'this' designation is optional. use it where
-        there is ambiguity or just to be complete and precise.
-        *********************************************/
-        return this->top < 0;
+    @param na : na
+    @return (bool) : true if empty, false if not
+    @exception na : na
+    @note the 'this' designation is optional. use it where
+    there is ambiguity or just to be complete and precise.
+    *********************************************/
+    return this->top < 0;
+}
+
+bool Stack::peek(int *peeki) {
+    /* ****************************************
+    Peek an item from the top of the stack
+    if there is one
+
+    @param (int*)) : a pointer to an integer to hold the peeked value
+    @return (bool) : true if peeked, false if not
+    @exception na   : na
+    @note this method does not throw an exception if the stack is empty.
+    It will return false in that case. If there is an item to peek,
+    it will be placed in the integer pointed to by the parameter.
+    *********************************************/
+    bool peeked = false;
+    if (top > -1) {
+        *peeki = stack[top];
+        peeked = true;
     }
+    return peeked;
+}
 
-    bool Stack::peek(int *peeki) {
-        /* ****************************************
-        Peek an item from the top of the stack
-        if there is one
+int Stack::pop() {
+    /* ****************************************
+    Pop an item from the top of the stack,
+    if there is one
 
-        @param (int*)) : a pointer to an integer to hold the peeked value
-        @return (bool) : true if peeked, false if not
-        @exception na : na
-        @note this method does not throw an exception if the stack is empty.
-        It will return false in that case. If there is an item to peek,
-        it will be placed in the integer pointed to by the parameter.
-        *********************************************/
-        bool peeked = false;
-        if (top > -1) {
-            *peeki = stack[top];
-            peeked = true;
-        }
-        return peeked;
+    @param na : na
+    @return (int) : the integer popped
+    @exception (int) : throw -1 if stack is empty
+    @note this method throws an exception if the stack is empty.
+    and will return the top item otherwise.
+    *********************************************/
+    if (top < 0) {
+        throw -1; // INSTRUCTIONAL: throw an int because we cannot return one
+    }             // INSTRUCTIONAL: no 'else' is needed, throw causes an immediate halt
+    return stack[top--];
+}
+
+bool Stack::push(int i) {
+    /* ****************************************
+    Push an item to the top of the stack,
+    if there is room
+
+    @param (int) i : any integer to push
+    @return (bool) : true if pushed, false if not
+    @exception na : na
+    @note na
+    *********************************************/
+    bool pushed = false;
+    if (top < STACKSIZE - 1) {
+        stack[++top] = i;
+        pushed = true;
     }
-
-    int Stack::pop() {
-        /* ****************************************
-        Pop an item from the top of the stack,
-        if there is one
-
-        @param na : na
-        @return (int) : the integer popped
-        @exception (int) : throw -1 if stack is empty
-        @note this method throws an exception if the stack is empty.
-        and will return the top item otherwise.
-        *********************************************/
-        if (top < 0) {
-            throw -1; // INSTRUCTIONAL: throw an int because we cannot return one
-        }             // INSTRUCTIONAL: no 'else' is needed, throw causes an immediate halt
-        return stack[top--];
-    }
-
-    bool Stack::push(int i) {
-        /* ****************************************
-        Push an item to the top of the stack,
-        if there is room
-
-        @param (int) i : any integer to push
-        @return (bool) : true if pushed, false if not
-        @exception na : na
-        @note na
-        *********************************************/
-        bool pushed = false;
-        if (top < STACKSIZE - 1) {
-            stack[++top] = i;
-            pushed = true;
-        }
-        return pushed;
-    }
+    return pushed;
+}
